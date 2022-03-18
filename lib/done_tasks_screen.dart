16988dfa1c1d2f022f9345732413825794d55bfb
Salma Essam <smsmacs666@gmail.com>
2022-03-18 20:07:25 +0200

    push final code

---
+++ lib/done_tasks_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'components.dart';
+import 'cubit.dart';
+import 'states.dart';
+
+class DoneTasksScreen extends StatelessWidget {
+  const DoneTasksScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocConsumer<AppCubit, AppStates>(
+      listener: (context, state) {},
+      builder: (context, state)
+      {
+        var tasks = AppCubit.get(context).doneTasks;
+
+        return tasksBuilder(
+          tasks: tasks,
+        );
+      },
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'home_layout.dart';
+
+void main()
+{
+  runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeLayout(),
+    ));
+}
+
